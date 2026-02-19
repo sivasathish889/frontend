11975aeaa43992bdc5f0b53965cae3c5cb759992
sivasathish889 <sivasathish889@gmail.com>
2026-02-19 19:38:00 +0530

    toast bug fixed

--- src/providers.tsx
+++ src/providers.tsx
@@ -4,6 +4,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from '@/context/AuthContext';
 import { useState } from 'react';
 
+import { Toaster } from 'sonner';
+
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
@@ -11,6 +13,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
           {children}
+          <Toaster />
       </AuthProvider>
     </QueryClientProvider>
   );
